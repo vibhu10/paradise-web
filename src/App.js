e09eb6e9e57a8f5939860c14fddc4ccf434d195c
vibhu10 <kumar.vibhu.id@gmail.com>
2024-09-30 16:32:19 +0530

    influencer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Host from './components/Host/Host'
-
+import  EdifInfluencerProfile from './components/influencer/Edif-Influencer-Profile';
+import Inbox from './components/influencer/Inbox'
 function App() {
   return (
     <Router>
@@ -12,7 +13,8 @@ function App() {
          
           <Route path="/" element={<Home />} /> 
           <Route path='/host' element={<Host />}/>
-        
+        <Route path='/influencer' element={<EdifInfluencerProfile/>}/>
+        <Route path='/influencer/inbox' element={<Inbox/>}/>
         </Routes>
       </div>
     </Router>
